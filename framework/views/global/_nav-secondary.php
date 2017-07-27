ed4fa35d49c09a9266f4bdaf8d0533fab6279ab8
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-07-26 21:32:31 -0600

    move login js/link to the "my account" link (secondary menu)

--- framework/views/global/_nav-secondary.php
+++ framework/views/global/_nav-secondary.php
@@ -20,6 +20,35 @@
     </div>
     <ul class="inline column right">
         <li><a href="<?php echo home_url(); ?>/my-teams/">My Teams</a></li>
-        <li><a href="<?php echo home_url(); ?>/my-account/">My Account</a></li>
+        <li>
+            <?php
+            if ( is_user_logged_in() ):
+                $first_name = get_user_meta( $current_user->ID, 'first_name', true );
+                $last_name  = get_user_meta( $current_user->ID, 'last_name', true );
+                ?>
+                <a class="profilenav_link" href="#">
+                    <span class="profilenav_dname">My Account</span>
+                </a>
+                <script>
+                    jQuery(document).ready(function () {
+                        jQuery(".profilenav_link").click(function () {
+                            jQuery(".pop-over").toggle("slow");
+                        });
+                        jQuery(".pop-over-header-close-btn").click(function () {
+                            jQuery(".pop-over").hide("slow");
+                        });
+                        jQuery(document).click(function (e) {
+                            if (e.target.class != 'pop-over' && !jQuery('.pop-over').find(e.target).length) {
+                                jQuery(".pop-over").hide("slow");
+                            }
+                        });
+                    });
+                </script>
+            <?php else: ?>
+                <a class="profilenav_link" href="<?php echo get_site_url(); ?>/profile/" style="margin-right:8px;">Log In</a>
+            <?php endif; ?>
+        </li>
+
+
     </ul>
 </nav>
--- framework/views/global/_navbar.php
+++ framework/views/global/_navbar.php
@@ -30,61 +30,6 @@ $sidebarAdSlot = "9635782928";
 
                 <div id="loginControls">
 
-                    <?php
-                    if ( is_user_logged_in() ):
-
-                        $first_name = get_user_meta( $current_user->ID, 'first_name', true );
-                        $last_name  = get_user_meta( $current_user->ID, 'last_name', true );
-
-                        ?>
-
-                        <a class="profilenav_link" href="#" style="margin-right:8px;"><span
-                                    class="profilenav_dname"><?php echo $current_user->display_name; ?></span> <i
-                                    class="fa fa-user fa-lg"></i></a>
-                        <div id="profilenav_con" class="pop-over">
-
-                            <div class="pop-over-header">
-                                <span class="pop-over-header-title"><?php echo ( ! empty( $first_name ) ) ? $first_name . ' ' . $last_name : $current_user->display_name; ?></span>
-                                <a href="#" class="pop-over-header-close-btn">x</a>
-                            </div>
-
-                            <div class="pop-over-content" style="max-height: 301px;">
-                                <div>
-                                    <ul class="pop-over-list">
-                                        <li><a href="<?php echo get_site_url(); ?>/profile/">My Profile</a></li>
-                                        <li><a href="/favorites">My Favorites</a></li>
-                                        <li>
-                                            <a href="<?php echo wp_logout_url( apply_filters( 'the_permalink', get_permalink() ) ); ?>">Logout</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <script>
-
-                            jQuery(document).ready(function () {
-                                jQuery(".profilenav_link").click(function () {
-                                    jQuery(".pop-over").toggle("slow");
-                                });
-
-                                jQuery(".pop-over-header-close-btn").click(function () {
-                                    jQuery(".pop-over").hide("slow");
-                                });
-
-                                jQuery(document).click(function (e) {
-                                    if (e.target.class != 'pop-over' && !jQuery('.pop-over').find(e.target).length) {
-                                        jQuery(".pop-over").hide("slow");
-                                    }
-                                });
-                            });
-
-                        </script>
-                        <?php
-                    else:
-                        ?><a class="profilenav_link" href="<?php echo get_site_url(); ?>/profile/"
-                             style="margin-right:8px;"><i class="fa fa-user fa-lg"></i></a><?php
-                    endif; ?>
                     <a href="<?php get_site_url(); ?>/search/" style="margin-right:8px;"><i
                                 class="fa fa-search fa-lg"></i></a>
 
