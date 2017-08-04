cf985deadf5e764fae53438294db7e48718c51d9
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-08-03 21:56:58 -0600

    file for, home - 'the beat' - make global

--- framework/views/integrity/template-home.php
+++ framework/views/integrity/template-home.php
@@ -20,7 +20,7 @@ $disable_page_title = get_post_meta( get_the_ID(), '_x_entry_disable_page_title'
 
             <div class="x-column x-sm x-2-4 content">
                 <h2>The Beat</h2>
-                <?php x_get_view( 'integrity', 'content', 'the-beat' ); ?>
+                <?php x_get_view( 'global', '_content', 'the-beat' ); ?>
 
                 <?php while ( have_posts() ) : the_post(); ?>
                     <?php x_get_view( 'integrity', 'content', 'page' ); ?>
@@ -34,85 +34,4 @@ $disable_page_title = get_post_meta( get_the_ID(), '_x_entry_disable_page_title'
     </div>
 
 
-		
-		<div class="container" style="margin-top:20px;">
-			<div class='all-posts-sidebar'>
-				<ul>
-					<li class="active">All Posts</li>
-					<li>All Favorites</li>
-					<li>Seattle Seahawks</li>
-					<li>Seattle Mariners</li>
-				</ul>
-			</div>
-			<div class='all-posts-content'>
-				<div class="row post arena-post">
-					<span class="headshot"><a href=""><img src="http://placehold.it/66x66"></a></span>
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >"Seahawks should be looking at drafting Boise State Bronco running back, Jeremy McNichols. This guy is a stud!"
-</a></span>
-						<span class="username">Arena Post by <a href="#user">Username</a> on <a href="">Seattle Seahawks</a> Yesterday</span>
-
-					</div>
-
-				</div>
-				<div class="row post article-post">
-					<span class="headshot"><a href=""><img src="http://placehold.it/66x66"></a></span>
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >Seattle’s “NO NAME” receivers
-</a></span>
-						<span class="username">Article by <a href="#user">DJHaze</a> on <a href="">Seattle Seahawks</a> on 11/11/16</span>
-
-					</div>
-
-				</div>
-				<div class="row post beat-post">
-
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >Seattle Seahawks vs. Miami Dolphins Preview Beat Article
-</a></span>
-						<span class="username">Beat by <a href="#user">Username</a> on <a href=""></a> Yesterday</span>
-
-					</div>
-
-				</div>
-
-				<div class="row post arena-post">
-					<span class="headshot"><a href=""><img src="http://placehold.it/66x66"></a></span>
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >"Seahawks should be looking at drafting Boise State Bronco running back, Jeremy McNichols. This guy is a stud!"
-</a></span>
-						<span class="username">Arena Post by <a href="#user">Username</a> on <a href="">Seattle Seahawks</a> Yesterday</span>
-
-					</div>
-
-				</div>
-				<div class="row post article-post">
-					<span class="headshot"><a href=""><img src="http://placehold.it/66x66"></a></span>
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >Seattle’s “NO NAME” receivers
-</a></span>
-						<span class="username">Article by <a href="#user">DJHaze</a> on <a href="">Seattle Seahawks</a> on 11/11/16</span>
-
-					</div>
-
-				</div>
-				<div class="row post beat-post">
-
-					<div class="post-text">
-						<span class="post-title"><a href="#postedPage" >Seattle Seahawks vs. Miami Dolphins Preview Beat Article
-</a></span>
-						<span class="username">Beat by <a href="#user">Username</a> on <a href=""></a> Yesterday</span>
-
-					</div>
-
-				</div>
-
-			</div>
-
-		</div>
-		<div style="clear:both;">&nbsp;</div>
-		</div>
-
-
-
-		<?php get_footer(); ?>
+    <?php get_footer(); ?>
