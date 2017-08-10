ba3ad09904ede5ea4e05a54f4acc85b7f7557eb6
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-08-10 13:18:51 -0600

    update write a beat template to be submit and create beats properly

--- template-write-a-beat.php
+++ template-write-a-beat.php
@@ -15,32 +15,70 @@
 
 get_header();
 
-$game = new WP_Query( array(
+$query = new WP_Query( array(
     'p' => (int) $_GET['game'],
     'post_type' => 'game',
     'posts_per_page' => 1,
 ) );
+$beat_types = [ 'preview', 'recap' ];
+$game = $query->get_posts();
+$game = $game[0];
 
+$teams = fsu_get_teams_from_game( $game->ID );
+
+$team_ids = [];
+foreach( $teams as $team )
+    $team_ids[] = $team->ID;
+
+
+
+if( isset($_POST['submit']) && $query->have_posts() ) {
+
+    $beat_type = in_array( $_POST['beat_type'], $beat_types ) ? $_POST['beat_type'] : '';
+    $team_id = in_array( $_POST['team_id'], $team_ids ) ? $_POST['team_id'] : '';
+    $ref_team_id = null;
+
+
+    $create_beat = create_beat( $beat_type, $game->ID, $team_id, $ref_team_id );
+
+}
     ?>
     <div class="<?php x_main_content_class(); ?>" role="main">
         <div class="x-container max width">
             <div class="x-column x-sm x-1-1 content">
 
             <?php
-            if( $game->have_posts() ) {
-                while( $game->have_posts() ) : $game->the_post();
+            if( $query->have_posts() ) {
+                while( $query->have_posts() ) : $query->the_post();
                     ?>
                     <h2><?php echo $post->post_title; ?></h2>
                     <h3>Write a Beat</h3>
+
+                    <?php
+
+                    if( isset($create_beat) && $create_beat ) {
+                        echo 'Beat created!';
+                    } else {
+                    ?>
 
 
                     <form action="" method="post" enctype="multipart/form-data">
                         <label for="beat_type">
                             Beat Type<br />
                             <select name="beat_type">
-                                <option value="preview">Preview</option>
-                                <option value="recap">Recap</option>
-
+                                <?php
+                                foreach( $beat_types as $beat_type )
+                                    echo sprintf( '<option value="%s">%s</option>', $beat_type, ucwords($beat_type) );
+                                ?>
+                            </select>
+                        </label>
+                        <label for="team">
+                            Team<br />
+                            <select name="team_id">
+                                <?php
+                                foreach( $teams as $team )
+                                    echo sprintf( '<option value="%s">%s</option>', $team->ID, $team->post_title );
+                                ?>
                             </select>
                         </label>
 
@@ -60,6 +98,8 @@ $game = new WP_Query( array(
                             <input type="submit" name="submit" value="POST" />
                         </div>
                     </form>
+
+                    <?php } ?>
 
                 <?php
                 endwhile;
