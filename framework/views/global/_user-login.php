220ac33aefc5fe1e28fa6aa7aa6994bb8423bd2c
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-08-17 00:21:14 -0600

    change close -> X

--- framework/views/global/_user-login.php
+++ framework/views/global/_user-login.php
@@ -1,6 +1,6 @@
 <?php if( !is_user_logged_in() ) { ?>
 <div id="login">
-    <a href="#" class="userpro-close-popup"><?php _e('Close','userpro'); ?></a>
+    <a href="#" class="userpro-close-popup"><?php _e('X','userpro'); ?></a>
     <?php echo do_shortcode( '[userpro template=login]' ); ?>
 </div>
 <?php } ?>
